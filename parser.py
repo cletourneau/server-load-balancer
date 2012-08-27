14618b5c0429c461d88a9fcaad4a07c85a3f04bb
Carl Letourneau <cletourneau@gmail.com>
2012-08-27 19:51:10 -0400

    Added to_json parsing Missing main entry point and we're done!

--- parser.py
+++ parser.py
@@ -1,24 +1,39 @@
 import json
+import string
 
 from server import *
 from virtual_machine import *
 
+ID_KEY = "id"
+SLOT_KEY = "cases"
+SERVER_KEY = "serveurs"
+VM_KEY = "machineVirtuelles"
+USAGE_PERCENTAGE_KEY = "pourcentageUtilisation"
+
 def create_server_from(item):
-  return Server(id=item['id'], slot_capacity=item['cases'])
+  return Server(id=item[ID_KEY], slot_capacity=item[SLOT_KEY])
 
 def create_vm_from(item):
-  return VirtualMachine(id=item['id'], size=item['cases'])
+  return VirtualMachine(id=item[ID_KEY], size=item[SLOT_KEY])
+
+def create_vm_json_for(vm):
+  return '{"%s":"%s","%s":%d}' % (ID_KEY, vm.id, SLOT_KEY, vm.size)
+
+def create_server_json_for(server):
+  json_vms = [create_vm_json_for(vm) for vm in server.vms]
+  return '{"%s":"%s","%s":%d,"%s":%s,"%s":[%s]}' %\
+         (ID_KEY, server.id, SLOT_KEY, server.slot_capacity, USAGE_PERCENTAGE_KEY, server.current_load_percentage, VM_KEY, string.join(json_vms, ","))
 
 class Parser(object):
   def __extract_json_object_from(self, json_string):
     return json.loads(json_string)
 
   def __extract_servers_from(self, json_object):
-    server_list = json_object['serveurs']
+    server_list = json_object[SERVER_KEY]
     return [create_server_from(item) for item in server_list]
 
   def __extract_vms_from(self, json_object):
-    vm_list = json_object['machineVirtuelles']
+    vm_list = json_object[VM_KEY]
     return [create_vm_from(item) for item in vm_list]
 
   def from_json(self, json_string):
@@ -26,3 +41,9 @@ class Parser(object):
     servers = self.__extract_servers_from(json_object)
     vms = self.__extract_vms_from(json_object)
     return (servers, vms)
+
+  def to_json(self, servers):
+    json_servers = [create_server_json_for(s) for s in servers]
+
+    json = '{"%s":[%s]}' % (SERVER_KEY, string.join(json_servers, ","))
+    return json
--- parser_test.py
+++ parser_test.py
@@ -9,7 +9,7 @@ class ParserTestCase(unittest.TestCase):
   def setUp(self):
     self.parser = Parser()
 
-  def test_noJSONServersAndVms_returnsEmptyArrays(self):
+  def test_from_json_noServersAndVms_returnsEmptyArrays(self):
     json_string = '{"serveurs":[], "machineVirtuelles":[]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -17,7 +17,7 @@ class ParserTestCase(unittest.TestCase):
     assert_that(servers, equal_to([]))
     assert_that(vms, equal_to([]))
 
-  def test_oneServer_returnsIt(self):
+  def test_from_json_oneServer_returnsIt(self):
     json_string = '{"serveurs":[{"id":"serveur1","cases":4}], "machineVirtuelles":[]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -25,7 +25,7 @@ class ParserTestCase(unittest.TestCase):
     assert_that(servers, has_length(1))
     assert_that(servers, contains_a_server_with(id="serveur1", slot_capacity=4))
 
-  def test_multipleServer_returnsThem(self):
+  def test_from_json_multipleServer_returnsThem(self):
     json_string = '{"serveurs":[{"id":"serveur1","cases":4}, {"id":"serveur2","cases":5}, {"id":"serveur3","cases":10}], "machineVirtuelles":[]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -35,7 +35,7 @@ class ParserTestCase(unittest.TestCase):
     assert_that(servers, contains_a_server_with(id="serveur2", slot_capacity=5))
     assert_that(servers, contains_a_server_with(id="serveur3", slot_capacity=10))
 
-  def test_oneVm_returnsIt(self):
+  def test_from_json_oneVm_returnsIt(self):
     json_string = '{"serveurs":[], "machineVirtuelles":[{"id":"VM1","cases":1}]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -43,7 +43,7 @@ class ParserTestCase(unittest.TestCase):
     assert_that(vms, has_length(1))
     assert_that(vms, contains_a_vm_with(id="VM1", size=1))
 
-  def test_multipleVm_returnsThem(self):
+  def test_from_json_multipleVm_returnsThem(self):
     json_string = '{"serveurs":[], "machineVirtuelles":[{"id":"VM1","cases":1}, {"id":"VM2","cases":4}, {"id":"VM3","cases":8}]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -53,7 +53,7 @@ class ParserTestCase(unittest.TestCase):
     assert_that(vms, contains_a_vm_with(id="VM2", size=4))
     assert_that(vms, contains_a_vm_with(id="VM3", size=8))
 
-  def test_iweb_json(self):
+  def test_from_json_iweb(self):
     json_string = '{"serveurs":[{"id":"serveur1","cases":4},{"id":"serveur2","cases":6}],"machineVirtuelles":[{"id":"VM1","cases":1},{"id":"VM2","cases":4},{"id":"VM3","cases":2}]}'
 
     (servers, vms) = self.parser.from_json(json_string)
@@ -67,6 +67,44 @@ class ParserTestCase(unittest.TestCase):
     assert_that(vms, contains_a_vm_with(id="VM1", size=1))
     assert_that(vms, contains_a_vm_with(id="VM2", size=4))
     assert_that(vms, contains_a_vm_with(id="VM3", size=2))
+
+  #
+
+  def test_to_json_noServer(self):
+    actual_json = self.parser.to_json([])
+
+    assert_that(actual_json, equal_to('{"serveurs":[]}'))
+
+  def test_to_json_oneEmptyServer(self):
+    server = Server(id="server1", slot_capacity=8)
+    actual_json = self.parser.to_json([server])
+
+    assert_that(actual_json, equal_to('{"serveurs":[{"id":"server1","cases":8,"pourcentageUtilisation":0,"machineVirtuelles":[]}]}'))
+
+  def test_to_json_twoEmptyServers(self):
+    server1 = Server(id="server1", slot_capacity=8)
+    server2 = Server(id="server2", slot_capacity=4)
+    actual_json = self.parser.to_json([server1, server2])
+
+    assert_that(actual_json, equal_to('{"serveurs":[{"id":"server1","cases":8,"pourcentageUtilisation":0,"machineVirtuelles":[]},{"id":"server2","cases":4,"pourcentageUtilisation":0,"machineVirtuelles":[]}]}'))
+
+  def test_to_json_oneServer_withOneVm(self):
+    server = Server(id="server1", slot_capacity=8)
+    server.add_vm(VirtualMachine(id="vm1", size=3))
+    actual_json = self.parser.to_json([server])
+
+    assert_that(actual_json, equal_to('{"serveurs":[{"id":"server1","cases":8,"pourcentageUtilisation":38,"machineVirtuelles":[{"id":"vm1","cases":3}]}]}'))
+
+  def test_to_json_iweb(self):
+    server1 = Server(id="serveur1", slot_capacity=4)
+    server1.add_vm(VirtualMachine(id="VM1", size=1))
+    server1.add_vm(VirtualMachine(id="VM3", size=2))
+
+    server2 = Server(id="serveur2", slot_capacity=6)
+    server2.add_vm(VirtualMachine(id="VM2", size=4))
+
+    actual_json = self.parser.to_json([server1, server2])
+    assert_that(actual_json, equal_to('{"serveurs":[{"id":"serveur1","cases":4,"pourcentageUtilisation":75,"machineVirtuelles":[{"id":"VM1","cases":1},{"id":"VM3","cases":2}]},{"id":"serveur2","cases":6,"pourcentageUtilisation":67,"machineVirtuelles":[{"id":"VM2","cases":4}]}]}'))
 
 if __name__ == '__main__':
   unittest.main()
